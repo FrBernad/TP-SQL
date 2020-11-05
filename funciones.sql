4b577da2432478dce09db3443cb691ff3a184c72
Francisco <frbernad@itba.edu.ar>
2020-11-05 16:22:12 -0300

    fixed bugs with encoding \encoding UTF8

--- funciones.sql
+++ funciones.sql
@@ -8,7 +8,7 @@ CREATE TABLE PROVINCIA
 (
     id_prov int not null primary key,
     id_pais int not null,
-    foreign key (id_pais) references pais on delete restrict
+    foreign key (id_pais) references PAIS on delete restrict
 );
 
 CREATE TABLE DEPARTAMENTO
@@ -16,7 +16,7 @@ CREATE TABLE DEPARTAMENTO
     id_departamento serial not null primary key,
     nombreDepto     text   not null unique,
     provincia       int    not null,
-    foreign key (provincia) references provincia on delete restrict
+    foreign key (provincia) references PROVINCIA on delete restrict
 );
 
 CREATE TABLE LOCALIDAD
@@ -25,25 +25,25 @@ CREATE TABLE LOCALIDAD
     nombre          text   not null,
     id_departamento int    not null,
     canthab         int,
-    foreign key (id_departamento) references departamento on delete restrict
+    foreign key (id_departamento) references DEPARTAMENTO on delete restrict
 );
 
 CREATE TABLE AUXILIAR
 (
     nombreLocalidad text,
     nombrePais      text,
-    idProv         int,
+    idProv          int,
     nombreDepto     text,
     canthab         int
 );
 
+
 CREATE OR REPLACE FUNCTION validateData() RETURNS TRIGGER AS
 $$
 DECLARE
-
-    auxIdPais  pais.id_pais%TYPE;
-    auxIdDepto departamento.id_departamento%TYPE;
-    auxIdProvincia  provincia.id_prov%TYPE;
+    auxIdPais      PAIS.id_pais%TYPE;
+    auxIdDepto     DEPARTAMENTO.id_departamento%TYPE;
+    auxIdProvincia PROVINCIA.id_prov%TYPE;
 
 BEGIN
 
@@ -53,10 +53,10 @@ BEGIN
         select id_pais into auxIdPais from pais where nombrePais = new.nombrePais;
     END IF;
 
-    select  id_prov into auxIdProvincia from provincia where id_prov = new.idProv;
+    select id_prov into auxIdProvincia from provincia where id_prov = new.idProv;
     if (auxIdProvincia is null) THEN
         auxIdProvincia = cast(new.idProv as integer);
-        insert into provincia values (auxIdProvincia , auxIdPais);
+        insert into provincia values (auxIdProvincia, auxIdPais);
     END IF;
 
     select id_departamento into auxIdDepto from departamento where nombreDepto = new.nombreDepto;
@@ -66,7 +66,7 @@ BEGIN
     END IF;
 
     INSERT INTO LOCALIDAD(nombre, id_departamento, canthab)
-    VALUES (new.nombreLocalidad, auxIdDepto, cast(new.canthab as integer));
+    VALUES (new.nombreLocalidad, auxIdDepto, cast(coalesce(new.canthab,0) as integer));
 
     return NULL;
 END
@@ -81,10 +81,13 @@ EXECUTE PROCEDURE validateData();
 COPY AUXILIAR (nombreLocalidad, nombrePais, idProv, nombreDepto, canthab) FROM 'C:\Users\Agustin\Desktop\Facultad\Tercero\Primer Cuatrimestre\Base de datos I\TP\TP-SQL\localidades.csv' WITH (FORMAT csv,HEADER TRUE);
 
 drop table LOCALIDAD;
-drop table departamento;
-drop table provincia;
-drop table pais;
+drop table DEPARTAMENTO;
+drop table PROVINCIA;
+drop table PAIS;
+drop table AUXILIAR;
+
 DROP FUNCTION validateData;
+
 DROP TRIGGER fillData ON AUXILIAR;
-drop table auxiliar;
+
 
