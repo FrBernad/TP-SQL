2e011df35cb01a249b9b7b93c2012e693af485bf
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-11-05 11:28:34 -0300

    add insert trigger

--- funciones.sql
+++ funciones.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pais
 (
 id_pais serial not null primary key,
-pais text not null
+nombre text not null unique
 );
 
 CREATE TABLE provincia
@@ -14,7 +14,7 @@ foreign key (id_pais) references pais on delete restrict
 CREATE TABLE departamento
 (
 id_departamento serial not null primary key,
-departamento text not null,
+departamento text not null unique,
 provincia int not null,
 foreign key (provincia) references provincia on delete restrict
 );
@@ -25,6 +25,42 @@ CREATE TABLE  LOCALIDAD
     nombre text not null,
     id_departamento int not null,
     canthab int,
-    foreign key (id_departamento) references departamento on delete restrict,
-)
+    foreign key (id_departamento) references departamento on delete restrict
+);
+
+CREATE TRIGGER fillData
+    BEFORE INSERT ON LOCALIDAD
+    FOR EACH ROW
+    EXECUTE PROCEDURE validateData();
+
+
+CREATE OR REPLACE FUNCTION validateData() RETURNS TRIGGER AS $$
+
+    DECLARE
+
+        idPais pais.id_pais%TYPE;
+        idDepto departamento.id_departamento%TYPE;
+        idProv provincia.provincia%TYPE;
+
+        BEGIN
+            select id_pais into idPais from pais where pais = new.pais;
+            if(idPais is null ) THEN
+                insert into pais(pais.pais) values(new.pais);
+            END IF;
+
+            select provincia into idProv from provincia where provincia = new.provincia;
+            if(idProv is null ) THEN
+                insert into provincia values(new.provincia,id_pais);
+            END IF;
+
+            select id_departamento into idDepto from departamento where departamento.departamento = new.departamento;
+            if(idDepto is null ) THEN
+                INSERT INTO DEPARTAMENTO(departamento.departamento, provincia) VALUES(new.departamento,idProv);
+            END IF;
+
+            INSERT INTO LOCALIDAD(nombre, id_departamento, canthab) VALUES (new.nombre,idDepto,new.canthab);
+
+        END;
+$$ LANGUAGE plpgsql;
+
 
