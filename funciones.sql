1127d58f89db717475dae262cefd3fe978c9d7b9
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-11-05 14:43:47 -0300

    add aux table

--- funciones.sql
+++ funciones.sql
@@ -30,16 +30,18 @@ CREATE TABLE LOCALIDAD
     foreign key (id_departamento) references departamento on delete restrict
 );
 
-CREATE TRIGGER fillData
-    BEFORE INSERT
-    ON LOCALIDAD
-    FOR EACH ROW
-EXECUTE PROCEDURE validateData();
+CREATE TABLE AUXILIAR
+(
+    nombreLocalidad text,
+    nombrePais  text,
+    idProv int,
+    nombreDepto text,
+    canthab int
+);
 
 
 CREATE OR REPLACE FUNCTION validateData() RETURNS TRIGGER AS
 $$
-
 DECLARE
 
     idPais  pais.id_pais%TYPE;
@@ -47,32 +49,44 @@ DECLARE
     idProv  provincia.id_prov%TYPE;
 
 BEGIN
-    select id_pais into idPais from pais where nombrePais = new.pais;
+    select id_pais into idPais from pais where nombrePais = new.nombrePais;
     if (idPais is null) THEN
-        insert into pais(nombrePais) values (new.pais);
+        insert into pais(nombrePais) values (new.nombrePais);
+        select id_pais into idPais from pais where nombrePais = new.nombrePais;
     END IF;
 
-    select provincia into idProv from provincia where id_prov = new.provincia;
+
+    select provincia into idProv from provincia where id_prov = new.idProv;
     if (idProv is null) THEN
-        insert into provincia values (new.provincia, id_pais);
+        insert into provincia values (new.idProv, idPais);
+        select provincia into idProv from provincia where id_prov = new.idProv;
     END IF;
 
-    select id_departamento into idDepto from departamento where nombreDepto = new.departamento;
+
+    select id_departamento into idDepto from departamento where nombreDepto = new.nombreDepto;
     if (idDepto is null) THEN
-        INSERT INTO DEPARTAMENTO(nombreDepto, provincia) VALUES (new.departamento, idProv);
+        INSERT INTO DEPARTAMENTO(nombreDepto, provincia) VALUES (new.nombreDepto, idProv);
+        select id_departamento into idDepto from departamento where nombreDepto = new.nombreDepto;
     END IF;
 
-    INSERT INTO LOCALIDAD(nombre, id_departamento, canthab) VALUES (new.nombre, idDepto, new.canthab);
+    INSERT INTO LOCALIDAD(nombre, id_departamento, canthab) VALUES (new.nombreLocalidad, idDepto, new.canthab);
 
     return NULL;
-END;
+END
 $$ LANGUAGE plpgsql;
 
-COPY LOCALIDAD FROM 'C:\Users\Agustin\Desktop\Facultad\Tercero\Primer Cuatrimestre\Base de datos I\TP\TP-SQL\localidades.csv' WITH (FORMAT csv);
+CREATE TRIGGER fillData
+    BEFORE INSERT
+    ON AUXILIAR
+    FOR EACH ROW
+EXECUTE PROCEDURE validateData();
+
+COPY AUXILIAR(nombreLocalidad, nombrePais, idProv, nombreDepto, canthab) FROM 'C:\Users\Agustin\Desktop\Facultad\Tercero\Primer Cuatrimestre\Base de datos I\TP\TP-SQL\localidades.csv' WITH (FORMAT csv,HEADER TRUE);
 
 drop table LOCALIDAD;
 drop table departamento;
 drop table provincia;
 drop table pais;
-
-
+drop table auxiliar;
+DROP TRIGGER fillData ON AUXILIAR;
+DROP FUNCTION validateData;
